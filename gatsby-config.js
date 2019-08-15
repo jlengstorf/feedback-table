84668a0fab7622bbe754c7282fa7b6973a3f7ec1
Jason Lengstorf <jason@lengstorf.com>
2019-08-15 13:30:07 -0700

    feat: feedback table

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,12 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+  plugins: [
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        url: "https://api.gatsbyjs.org/public",
+        typeName: "GatsbyFeedback",
+        fieldName: "gatsby",
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,82 @@
 import React from "react"
+import { graphql } from "gatsby"
+import ReactTable from "react-table"
+import mean from "lodash.mean"
 
-export default () => <div>Hello world!</div>
+import "react-table/react-table.css"
+
+export const query = graphql`
+  {
+    gatsby {
+      feedback: getFeedback {
+        comment
+        url: originUrl
+        rating
+        date
+      }
+    }
+  }
+`
+
+export default ({ data }) => {
+  const feedback = data.gatsby.feedback.map(rating => ({
+    ...rating,
+    url: rating.url
+      .replace(/https:\/\/(www\.)?gatsbyjs.org/, "")
+      .replace(/\/$/, ""),
+    date: new Date(rating.date),
+  }))
+  return (
+    <>
+      <h1>Docs Feedback</h1>
+      <ReactTable
+        data={feedback}
+        columns={[
+          {
+            Header: "Feedback",
+            columns: [
+              {
+                Header: "URL",
+                id: "url",
+                accessor: d =>
+                  d.url.replace(/https:\/\/(www\.)?gatsbyjs.org/, ""),
+                filterMethod: (filter, row) =>
+                  row[filter.id].includes(filter.value),
+              },
+              {
+                Header: "Rating",
+                accessor: "rating",
+                aggregate: vals => mean(vals),
+                Aggregated: row => <span>{row.value.toFixed(1)} (avg)</span>,
+                width: 75,
+                filterable: false,
+              },
+              {
+                Header: "Comment",
+                accessor: "comment",
+                aggregate: () => 1,
+                Aggregated: () => <span>expand to read comments</span>,
+                filterable: false,
+              },
+              {
+                Header: "Date",
+                id: "date",
+                accessor: d => d.date.toLocaleDateString(),
+                aggregate: vals =>
+                  vals
+                    .map(d => new Date(d))
+                    .sort((a, b) => b.getTime() - a.getTime())[0]
+                    .toLocaleDateString(),
+                Aggregated: row => <span>Last updated: {row.value}</span>,
+                filterable: false,
+              },
+            ],
+          },
+        ]}
+        pivotBy={["url"]}
+        className="-striped -highlight"
+        filterable
+      />
+    </>
+  )
+}
